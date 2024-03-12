442fe2521920610914f69f0ec6acddf665612edb
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-03-12 18:41:41 +0000

    Test out flushing

--- training/training_loop.py
+++ training/training_loop.py
@@ -463,6 +463,7 @@ def training_loop(
             fields = dict(stats_dict, timestamp=timestamp)
             with stats_jsonl as f:
                 f.write(json.dumps(fields) + '\n')
+                f.flush()
             #stats_jsonl.write(json.dumps(fields) + '\n')
             #stats_jsonl.flush()
         if stats_tfevents is not None:
