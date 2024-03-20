9024b7d338bc790b47370e7afade64077906c954
Aaron Gokaslan <aaronGokaslan@gmail.com>
2024-03-20 17:24:38 +0000

    Reduce unnecessary GPU overhead

--- training/training_loop.py
+++ training/training_loop.py
@@ -25,7 +25,6 @@ from torch_utils.ops import conv2d_gradfix
 from torch_utils.ops import grid_sample_gradfix
 
 import legacy
-import tensorflow_io
 from metrics import metric_main
 
 def cosine_decay_with_warmup(cur_nimg, base_value, total_nimg, final_value=0.0, warmup_value=0.0, warmup_nimg=0, hold_base_value_nimg=0):
@@ -269,6 +268,7 @@ def training_loop(
         stats_jsonl = fsspec.open(os.path.join(run_dir, 'stats.jsonl'), 'wt').open()
         #stats_jsonl = None # TODO fix.
         try:
+            import tensorflow_io
             import torch.utils.tensorboard as tensorboard
             stats_tfevents = tensorboard.SummaryWriter(run_dir)
         except ImportError as err:
