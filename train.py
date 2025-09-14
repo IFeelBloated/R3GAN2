b3a89528659b5e6ef5b12833379578b1731400f6
Nick Huang <nickgray0@gmail.com>
2025-09-14 16:03:07 -0700

    smaller lr for imgnet, enable mild augmentation

--- train.py
+++ train.py
@@ -305,8 +305,8 @@ def main(**kwargs):
        
         decay_nimg = 2e8
        
-        c.aug_scheduler = { 'base_value': 0, 'final_value': 0, 'total_nimg': decay_nimg }
-        c.lr_scheduler = { 'base_value': 1e-2, 'final_value': 1e-3, 'total_nimg': decay_nimg }
+        c.aug_scheduler = { 'base_value': 0, 'final_value': 0.2, 'total_nimg': decay_nimg }
+        c.lr_scheduler = { 'base_value': 2.5e-3, 'final_value': 1e-3, 'total_nimg': decay_nimg }
         c.gamma_scheduler = { 'base_value': 0.5, 'final_value': 0.05, 'total_nimg': decay_nimg }
         c.beta2_scheduler = { 'base_value': 0.9, 'final_value': 0.99, 'total_nimg': decay_nimg }
 
@@ -320,8 +320,8 @@ def main(**kwargs):
         
         decay_nimg = 2e8
         
-        c.aug_scheduler = { 'base_value': 0, 'final_value': 0, 'total_nimg': decay_nimg }
-        c.lr_scheduler = { 'base_value': 1e-2, 'final_value': 1e-3, 'total_nimg': decay_nimg }
+        c.aug_scheduler = { 'base_value': 0, 'final_value': 0.2, 'total_nimg': decay_nimg }
+        c.lr_scheduler = { 'base_value': 2.5e-3, 'final_value': 1e-3, 'total_nimg': decay_nimg }
         c.gamma_scheduler = { 'base_value': 1, 'final_value': 0.1, 'total_nimg': decay_nimg }
         c.beta2_scheduler = { 'base_value': 0.9, 'final_value': 0.99, 'total_nimg': decay_nimg }
 
