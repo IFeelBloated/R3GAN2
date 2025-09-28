c5df47741bbe3ae572d0428cf78de6304bf5fc39
Nick Huang <nickgray0@gmail.com>
2025-09-28 16:15:52 -0700

    raise gamma for imgnet

--- train.py
+++ train.py
@@ -307,7 +307,7 @@ def main(**kwargs):
        
         c.aug_scheduler = { 'base_value': 0, 'final_value': 0.4, 'total_nimg': decay_nimg }
         c.lr_scheduler = { 'base_value': 1e-2, 'final_value': 1e-3, 'total_nimg': decay_nimg }
-        c.gamma_scheduler = { 'base_value': 0.05 * 2, 'final_value': 0.005 * 2, 'total_nimg': decay_nimg }
+        c.gamma_scheduler = { 'base_value': 0.5, 'final_value': 0.05, 'total_nimg': decay_nimg }
         c.beta2_scheduler = { 'base_value': 0.9, 'final_value': 0.99, 'total_nimg': decay_nimg }
 
     if opts.preset == 'ImageNet-64':
@@ -322,7 +322,7 @@ def main(**kwargs):
         
         c.aug_scheduler = { 'base_value': 0, 'final_value': 0.4, 'total_nimg': decay_nimg }
         c.lr_scheduler = { 'base_value': 1e-2, 'final_value': 1e-3, 'total_nimg': decay_nimg }
-        c.gamma_scheduler = { 'base_value': 0.05 * 4, 'final_value': 0.005 * 4, 'total_nimg': decay_nimg }
+        c.gamma_scheduler = { 'base_value': 2, 'final_value': 0.2, 'total_nimg': decay_nimg }
         c.beta2_scheduler = { 'base_value': 0.9, 'final_value': 0.99, 'total_nimg': decay_nimg }
 
     c.G_kwargs.NoiseDimension = NoiseDimension
