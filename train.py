a438d450dcc82268d85683804cce48bcdcc52869
Nick Huang <nickgray0@gmail.com>
2025-10-03 14:09:44 -0700

    XL config

--- train.py
+++ train.py
@@ -307,7 +307,7 @@ def main(**kwargs):
        
         c.aug_scheduler = { 'base_value': 0, 'final_value': 0.4, 'total_nimg': decay_nimg }
         c.lr_scheduler = { 'base_value': 1e-2, 'final_value': 1e-3, 'total_nimg': decay_nimg }
-        c.gamma_scheduler = { 'base_value': 0.5, 'final_value': 0.05, 'total_nimg': decay_nimg }
+        c.gamma_scheduler = { 'base_value': 0.1, 'final_value': 0.01, 'total_nimg': decay_nimg }
         c.beta2_scheduler = { 'base_value': 0.9, 'final_value': 0.99, 'total_nimg': decay_nimg }
 
     if opts.preset == 'ImageNet-64':
@@ -322,7 +322,22 @@ def main(**kwargs):
         
         c.aug_scheduler = { 'base_value': 0, 'final_value': 0.4, 'total_nimg': decay_nimg }
         c.lr_scheduler = { 'base_value': 1e-2, 'final_value': 1e-3, 'total_nimg': decay_nimg }
-        c.gamma_scheduler = { 'base_value': 2, 'final_value': 0.2, 'total_nimg': decay_nimg }
+        c.gamma_scheduler = { 'base_value': 0.4, 'final_value': 0.04, 'total_nimg': decay_nimg }
+        c.beta2_scheduler = { 'base_value': 0.9, 'final_value': 0.99, 'total_nimg': decay_nimg }
+
+    if opts.preset == 'ImageNet-32XL':
+        WidthPerStage = [x * 3 for x in [1024, 1024, 1024]]
+        BlocksPerStage = [['FFN', 'FFN', 'FFN', 'FFN'], ['FFN', 'FFN', 'FFN', 'FFN'], ['FFN', 'FFN', 'FFN', 'FFN']]
+        NoiseDimension = 64
+       
+        c.G_kwargs.ClassEmbeddingDimension = NoiseDimension
+        c.D_kwargs.ClassEmbeddingDimension = WidthPerStage[0]
+       
+        decay_nimg = 2e8
+       
+        c.aug_scheduler = { 'base_value': 0, 'final_value': 0.45, 'total_nimg': decay_nimg }
+        c.lr_scheduler = { 'base_value': 1e-2, 'final_value': 1e-3, 'total_nimg': decay_nimg }
+        c.gamma_scheduler = { 'base_value': 0.1, 'final_value': 0.01, 'total_nimg': decay_nimg }
         c.beta2_scheduler = { 'base_value': 0.9, 'final_value': 0.99, 'total_nimg': decay_nimg }
 
     c.G_kwargs.NoiseDimension = NoiseDimension
@@ -331,18 +346,18 @@ def main(**kwargs):
     c.G_kwargs.BlocksPerStage = BlocksPerStage
     c.G_kwargs.MLPWidthRatio = 2
     c.G_kwargs.FFNWidthRatio = 2
-    c.G_kwargs.ChannelsPerConvolutionGroup = 32
+    c.G_kwargs.ChannelsPerConvolutionGroup = 32 * WidthPerStage[0] // 512
     c.G_kwargs.AttentionWidthRatio = 1
-    c.G_kwargs.ChannelsPerAttentionHead = 64
+    c.G_kwargs.ChannelsPerAttentionHead = 64 * WidthPerStage[0] // 512
     
     c.D_kwargs.ModulationDimension = WidthPerStage[0] if opts.cond else None
     c.D_kwargs.WidthPerStage = [*reversed(WidthPerStage)]
     c.D_kwargs.BlocksPerStage = [*reversed(BlocksPerStage)]
     c.D_kwargs.MLPWidthRatio = 2
     c.D_kwargs.FFNWidthRatio = 2
-    c.D_kwargs.ChannelsPerConvolutionGroup = 32
+    c.D_kwargs.ChannelsPerConvolutionGroup = 32 * WidthPerStage[0] // 512
     c.D_kwargs.AttentionWidthRatio = 1
-    c.D_kwargs.ChannelsPerAttentionHead = 64
+    c.D_kwargs.ChannelsPerAttentionHead = 64 * WidthPerStage[0] // 512
     
     
     c.metrics = opts.metrics
